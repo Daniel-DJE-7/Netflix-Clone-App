0fb6f239a427c8cdae776eef34e8c9f3c480bd47
memo figueredo <memof80@gmail.com>
2026-01-30 17:13:06 -0500

    [reorganize the files of the project] new groups were created in the navigator named core and general within controllers.

---
+++ Netflix Clone/Controllers/General/SearchResultsViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SearchResultsViewController.swift
+//  Netflix Clone
+//
+//  Created by Memo Figueredo on 30/1/26.
+//
+
+import UIKit
+
+class SearchResultsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+      view.backgroundColor = .systemYellow
+    }
+    
+
+
+}
